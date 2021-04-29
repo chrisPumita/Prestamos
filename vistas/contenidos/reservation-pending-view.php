5e99a00a3e124d9f652b441acf14d556f7139119
Christan RCSG <christian.floppy@gmail.com>
2021-04-29 18:03:40 -0500

    FR - Vistas actualizadas.
    Plantilla lista y administrada por clase model vista

--- vistas/contenidos/reservation-pending-view.php
+++ vistas/contenidos/reservation-pending-view.php
@@ -10,19 +10,19 @@
 <div class="container-fluid">
     <ul class="full-box list-unstyled page-nav-tabs">
         <li>
-            <a href="reservation-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; NUEVO PRÉSTAMO</a>
+            <a href="<?php echo SERVERURL; ?>reservation-new.html"><i class="fas fa-plus fa-fw"></i> &nbsp; NUEVO PRÉSTAMO</a>
         </li>
         <li>
-            <a href="reservation-reservation.html"><i class="far fa-calendar-alt"></i> &nbsp; RESERVACIONES</a>
+            <a href="<?php echo SERVERURL; ?>reservation-reservation.html"><i class="far fa-calendar-alt"></i> &nbsp; RESERVACIONES</a>
         </li>
         <li>
-            <a class="active" href="reservation-pending.html"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; PRÉSTAMOS</a>
+            <a class="active" href="<?php echo SERVERURL; ?>reservation-pending.html"><i class="fas fa-hand-holding-usd fa-fw"></i> &nbsp; PRÉSTAMOS</a>
         </li>
         <li>
-            <a href="reservation-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; FINALIZADOS</a>
+            <a href="<?php echo SERVERURL; ?>reservation-list.html"><i class="fas fa-clipboard-list fa-fw"></i> &nbsp; FINALIZADOS</a>
         </li>
         <li>
-            <a href="reservation-search.html"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; BUSCAR POR FECHA</a>
+            <a href="<?php echo SERVERURL; ?>reservation-search.html"><i class="fas fa-search-dollar fa-fw"></i> &nbsp; BUSCAR POR FECHA</a>
         </li>
     </ul>
 </div>
@@ -57,7 +57,7 @@
                     </a>
                 </td>
                 <td>
-                    <a href="reservation-update.html" class="btn btn-success">
+                    <a href="<?php echo SERVERURL; ?>reservation-update.html" class="btn btn-success">
                         <i class="fas fa-sync-alt"></i>
                     </a>
                 </td>
@@ -82,7 +82,7 @@
                     </a>
                 </td>
                 <td>
-                    <a href="reservation-update.html" class="btn btn-success">
+                    <a href="<?php echo SERVERURL; ?>reservation-update.html" class="btn btn-success">
                         <i class="fas fa-sync-alt"></i>
                     </a>
                 </td>
@@ -107,7 +107,7 @@
                     </a>
                 </td>
                 <td>
-                    <a href="reservation-update.html" class="btn btn-success">
+                    <a href="<?php echo SERVERURL; ?>reservation-update.html" class="btn btn-success">
                         <i class="fas fa-sync-alt"></i>
                     </a>
                 </td>
@@ -132,7 +132,7 @@
                     </a>
                 </td>
                 <td>
-                    <a href="reservation-update.html" class="btn btn-success">
+                    <a href="<?php echo SERVERURL; ?>reservation-update.html" class="btn btn-success">
                         <i class="fas fa-sync-alt"></i>
                     </a>
                 </td>
